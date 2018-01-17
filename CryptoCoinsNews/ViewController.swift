19f251c3606e3a3f32f044f45965a64af7265d67
Alaattin Bedir <alaattinbedir@hotmail.com>
2018-01-17 19:44:15 +0300

    Initial Commit

---
+++ CryptoCoinsNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoCoinsNews
+//
+//  Created by Alaattin Bedir on 17.01.2018.
+//  Copyright © 2018 magiclampgames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
