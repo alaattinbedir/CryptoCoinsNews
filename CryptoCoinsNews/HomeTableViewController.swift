b04469d25beda263ced934b07e0d15ecb033b3b9
Alaattin Bedir <alaattinbedir@hotmail.com>
2018-01-21 13:25:43 +0300

    amend

--- CryptoCoinsNews/HomeTableViewController.swift
+++ CryptoCoinsNews/HomeTableViewController.swift
@@ -20,11 +20,17 @@ class HomeTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        // Setting tableView
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 66.0
+        self.tableView.separatorStyle = .none
+        
         // Get messages from service
         ArticlesService.sharedInstance.getArticles(completion: { (articles) in
             self.articlesArray = articles
             if self.self.articlesArray.count > 0 {
-//                self.tableView.reloadData()
+                self.tableView.reloadData()
             }
         }) { (code, error) in
             self.showMessage(message: error)
@@ -32,6 +38,28 @@ class HomeTableViewController: UITableViewController {
         
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let article = self.articlesArray[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath) as! NewsCellTableViewCell
+        
+        cell.titleLabel.text = article.title!
+        cell.descriptionLabel.text = article.description!
+        cell.newsImageView.loadImageUsingCache(withUrl: article.urlToImage!)
+        
+        return cell
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articlesArray.count
+    }
+    
+    
     fileprivate func showMessage(message : String) {
         let alert = UIAlertController(title: "Alert",
                                       message: message,
@@ -62,14 +90,35 @@ class HomeTableViewController: UITableViewController {
 
 }
 
-//class CustomServerTrustPoliceManager : ServerTrustPolicyManager {
-//    override func serverTrustPolicy(forHost host: String) -> ServerTrustPolicy? {
-//        return .disableEvaluation
-//    }
-//    public init() {
-//        super.init(policies: [:])
-//    }
-//}
+let imageCache = NSCache<NSString, AnyObject>()
 
+extension UIImageView {
+    func loadImageUsingCache(withUrl urlString : String) {
+        let url = URL(string: urlString)
+        self.image = nil
+        
+        // check cached image
+        if let cachedImage = imageCache.object(forKey: urlString as NSString) as? UIImage {
+            self.image = cachedImage
+            return
+        }
+        
+        // if not, download image from url
+        URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            DispatchQueue.main.async {
+                if let image = UIImage(data: data!) {
+                    imageCache.setObject(image, forKey: urlString as NSString)
+                    self.image = image
+                }
+            }
+            
+        }).resume()
+    }
+}
 
 
---
+++ CryptoCoinsNews/Views/Cells/NewsCellTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  NewsCellTableViewCell.swift
+//  CryptoCoinsNews
+//
+//  Created by Alaattin Bedir on 21.01.2018.
+//  Copyright © 2018 magiclampgames. All rights reserved.
+//
+
+import UIKit
+
+class NewsCellTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var newsImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
